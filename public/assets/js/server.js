0ec372091c14c90ee4d2a7a6b89fc87d2d5d2b28
Derek Meduri <derekmeduri@gmail.com>
2023-07-18 11:12:48 -0400

    adding to server js file

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
+const PORT = 3001;
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.listen(PORT, function () {
+  console.log(`server is listening on PORT: ${PORT}`);
+});
