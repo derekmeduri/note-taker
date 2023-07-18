dcf4f4ea62c920cda866af26a8722d5980971687
Derek Meduri <derekmeduri@gmail.com>
2023-07-18 11:17:34 -0400

    adding middleware to server js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -4,6 +4,9 @@ const PORT = 3001;
 
 const app = express();
 
+//Middleware for parsing json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.listen(PORT, function () {
