f4537de0a5e0cdf48d9e0835681bf894b03b7ec3
Derek Meduri <derekmeduri@gmail.com>
2023-07-20 19:36:28 -0400

    adding functions to api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,21 +1,33 @@
 //required libraries
 const router = require("express").Router();
+const { response } = require("express");
 const store = require("../db/store");
 
 //API GET request to get notes from db
 router.get("/notes", (req, res) => {
-  store;
+  store
+    .getNote()
+    .then((response) => {
+      return res.json(response);
+    })
+    .catch((error) => res.status(500).json(error));
 });
 
 //API POST reuqest to add note to db
 router.post("/notes", (req, res) => {
-  store;
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((error) => res.status(500).json(error));
 });
 
 //API DELETE request
-router.delete("", (req, res) => {
-  store;
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((error) => res.status(500).json(error));
 });
 
-//exporting router to
+//exporting router to server
 module.exports = router;
